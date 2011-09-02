2fcc901f2ac2c9172d2bc350c41ae67b433ce711
Al Snow <jasnow@hotmail.com>
2011-09-02 16:14:27 -0400

    Fixed cucumber run problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'ede45c9e49312c00a40e23854f5c5ca024a08d1cce40eec3c88916366160b0f9fb22a7718b6edd727b8648688e952cd5a583e1b01068457c5c0414bb3d359dbc'
